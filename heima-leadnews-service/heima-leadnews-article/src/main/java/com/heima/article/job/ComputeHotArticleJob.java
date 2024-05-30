3b2e22350ced51e12239f5f4d455b804b95e80d5
liuyuzhe <15313482685@163.com>
2024-05-30 09:27:38 +0800

    xxl-job 扫描 热文章 存到Redis

---
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/job/ComputeHotArticleJob.java
@@ -0,0 +1,26 @@
+package com.heima.article.job;
+
+import com.heima.article.service.HotArticleService;
+import com.xxl.job.core.handler.annotation.XxlJob;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+@Slf4j
+public class ComputeHotArticleJob {
+
+    @Autowired
+    private HotArticleService hotArticleService;
+
+    @XxlJob("computeHotArticleJob")
+    public void computeHotArticleJob() {
+        log.info("热点文章执行计算");
+        hotArticleService.computeHotArticle();
+    }
+
+}
